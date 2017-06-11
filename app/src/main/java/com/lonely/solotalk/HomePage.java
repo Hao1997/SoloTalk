b04e6387b9f0196c6d8afd61524e3088b41c8a98
Hao1997 <hluo072@uottawa.ca>
2017-06-10 22:32:17 -0400

    Added Menu

--- app/src/main/java/com/lonely/solotalk/HomePage.java
+++ app/src/main/java/com/lonely/solotalk/HomePage.java
@@ -1,10 +1,13 @@
 package com.lonely.solotalk;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.FrameLayout;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
@@ -21,8 +24,14 @@ public class HomePage extends DrawerActivityCustom {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.activity_home_page);
-        super.setUpNavigationBar();
+        //setContentView(R.layout.activity_home_page);
+        // super.setContentView(R.layout.drawer_custom);
+        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.frame_container);
+        // inflate the custom activity layout
+        LayoutInflater layoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View activityView = layoutInflater.inflate(R.layout.activity_home_page, null,false);
+        // add the custom layout of this activity to frame layout.
+        frameLayout.addView(activityView);
     }
 
 }
